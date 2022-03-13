02404abcda8919dfb84ab5e3ac73fc19ad3405a2
Alvaro <alvaro.saburido@gmail.com>
2022-03-13 19:31:48 +0100

    fix: typescript issues

--- src/core/gui.ts
+++ src/core/gui.ts
@@ -1,6 +1,11 @@
-import { Pane } from 'tweakpane'
+import { BladeApi, Pane } from 'tweakpane'
 import * as EssentialsPlugin from '@tweakpane/plugin-essentials'
+import { BladeController, View } from '@tweakpane/core'
 
+interface FPSGraph extends BladeApi<BladeController<View>> {
+  begin(): void
+  end(): void
+}
 // Debug
 export const gui = new Pane()
 gui.registerPlugin(EssentialsPlugin)
@@ -8,4 +13,4 @@ gui.registerPlugin(EssentialsPlugin)
 export const fpsGraph = gui.addBlade({
   view: 'fpsgraph',
   label: 'fpsgraph',
-})
+}) as FPSGraph
